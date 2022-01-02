ec82a0504ec94e66988aa141beeb4e51f7de6c48
Gapur Yovloy <kaloycant@gmail.com>
2022-01-02 22:31:32 +0300

    #1 finished Color Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,29 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+let counter = 0;
+
+btn.addEventListener("click", () => {
+  if (counter < colors.length) {
+    document.body.style.backgroundColor = colors[counter];
+    color.textContent = colors[counter];
+    counter++;
+
+    if (counter === colors.length) {
+      counter = 0;
+    }
+  }
+});
+
+// btn.addEventListener("click", () => {
+//   let currentColorIndex = colors.findIndex(
+//     (c) => c == document.body.style.backgroundColor
+//   );
+//   console.log({ currentColorIndex });
+//   currentColorIndex =
+//     currentColorIndex <= colors.length  ? currentColorIndex + 1 : 0;
+//   console.log(document.body.style.backgroundColor);
+//   document.body.style.backgroundColor = colors[currentColorIndex];
+//   color.textContent = colors[currentColorIndex];
+// });
