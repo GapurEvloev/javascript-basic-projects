4a24821cd774264e4b53be2fba03e9c2f22c3efd
Gapur Yovloy <kaloycant@gmail.com>
2022-01-03 13:38:12 +0300

    #4 finished

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const linksList = document.querySelector(".links-list");
+
+navToggle.addEventListener("click", function () {
+    let size = (linksList.clientHeight / 16) + "rem";
+    links.style.setProperty("--links-height", size);
+    links.classList.toggle("show-links");
+})
