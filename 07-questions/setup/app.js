36195ea9ecd948c9e9a281b93fafb5cd4d32a928
Gapur Yovloy <kaloycant@gmail.com>
2022-01-03 14:57:46 +0300

    #7 accordion questions finished

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,22 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+    const btn = question.querySelector(".question-btn");
+    const qWrapper = question.querySelector(".question-text-wrap");
+    const qText = question.querySelector(".question-text");
+
+    btn.addEventListener("click", (e) => {
+        // let rect = qText.getBoundingClientRect().height; // - более точное значение высоты
+        let size = (qText.clientHeight / 16) + "rem";
+        qWrapper.style.setProperty("--question-height", size);
+
+        questions.forEach((item) => {
+            if(item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+})
